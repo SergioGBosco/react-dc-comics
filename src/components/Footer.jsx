6001a30decef8b0fc37423bb8816e3c68ce67514
SergioGBosco <sergiogibosco@gmail.com>
2025-08-26 17:50:39 +0200

    feat: add layout footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer del sito
-    </div>
+    <footer>
+      <div className="banner">
+        <div className="container">
+          <div className="row">
+            <div className="col-100">
+              <div className="icons-list"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="top-footer"></div>
+      <div className="bottom-footer"></div>
+    </footer>
   )
 }
 
