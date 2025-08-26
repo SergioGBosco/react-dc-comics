0a1b2e52b73f5f83471be8acea52fe88a0fbad1c
SergioGBosco <sergiogibosco@gmail.com>
2025-08-26 18:23:58 +0200

    fix: add img,description, css for banner footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,39 @@ const Footer = () => {
         <div className="container">
           <div className="row">
             <div className="col-100">
-              <div className="icons-list"></div>
+              <div className="icons-list">
+                <div>
+                  <img src="../../public/img/buy-comics-digital-comics.png" alt="" />
+                  <span>digital comics</span>
+                </div>
+                <div>
+                  <img src="../../public/img/buy-comics-merchandise.png" alt="" />
+                  <span>dc merchandise</span>
+                </div>
+                <div>
+                  <img src="../../public/img/buy-comics-subscriptions.png" alt="" />
+                  <span>subscriptions</span>
+                </div>
+                <div>
+                  <img src="../../public/img/buy-comics-shop-locator.png" alt="" />
+                  <span>comic shop locator</span>
+                </div>
+                <div>
+                  <img src="../../public/img/buy-comics-shop-locator.png" alt="" />
+                  <span>dc power visa</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="top-footer"></div>
+      <div className="top-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-30"></div>
+          </div>
+        </div>
+      </div>
       <div className="bottom-footer"></div>
     </footer>
   )
